998b6e24a046050b8bb2ab5b46391187110f098b
Michael Chalupa <mchalupa3@gmail.com>
2019-04-11 12:43:08 -0500

    update

--- untitled/src/RandNum.java
+++ untitled/src/RandNum.java
@@ -1,18 +1,27 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class RandNum {
     public static void main(String[] args){
+        Scanner scan = new Scanner( System.in );
 
 
 
         Random rand = new Random();
 
-        int n = rand.nextInt(50);
+        int n = rand.nextInt(11);
 
 
         n += 1;
 
-        System.out.println( n );
+        System.out.println( "Guess a number 1-10" );
+        int randNumGuess = scan.nextInt();
+        if(randNumGuess == n ){
+            System.out.println( "Correct!" );
+        }
+        else{
+            System.out.println( "You suck the number was " + n );
+        }
 
     }
 }
